dd572825ebac7b80f743c804c81904f02a94337b
marcelolima <marcelolima@ic.ufal.br>
2018-04-18 10:31:59 -0300

    First commit

---
+++ main.c
@@ -0,0 +1,50 @@
+/*
+ * This is a prototype 'main.c' for the Huffman Project
+ * Data Structures class, Professor Marcio Ribeiro
+ * Made by:
+ * Bruno Rafael Severo
+ * Marcelo Lima
+ * Thiago Miranda
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+// Receives an error code and prints an error message
+void argumentError(int e){
+	switch (e){
+		case 1:
+			printf("Please use 3 arguments as follows:\n\n");
+			printf("Example 1: ./huffman compress picture.png pic.huff\n");
+			printf("Example 2: ./huffman uncompress pic.huff picture.png\n");
+			printf("\nEnding program.\n");
+			break;
+		case 2:
+			printf("Invalid arguments.\n");
+			break;
+	}
+}
+
+int main(int argc, char const *argv[]){
+
+	/* 
+	 * If the number of arguments is different than 3,
+	 * prints out instructions and ends the program.
+	*/
+	if (argc != 4){
+		argumentError(1);
+		return 0;
+	}
+
+	if ( !strcmp(argv[1], "compress") ){
+		printf("Compressing file...-\n");
+		//compress(argv[2], argv[3]);
+	}
+	else if ( !strcmp(argv[1], "uncompress") ){
+		printf("Uncompressing file...\n");
+		//uncompress(argv[2], argv[3]);
+	}
+	else  //Invalid argument
+		argumentError(2);
+	return 0;
+}
