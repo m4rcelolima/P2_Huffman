ec1ce9ddde7d3d591820f9830b79fdf36b5f5de5
marcelolima <marcelolima@ic.ufal.br>
2018-04-27 23:10:22 -0300

    Change 'uncompress' to 'decompress'
    The correct term is decompress.

--- main.c
+++ main.c
@@ -16,7 +16,7 @@ void argumentError(int e){
 		case 1:
 			printf("Please use 3 arguments as follows:\n\n");
 			printf("Example 1: ./huffman compress picture.png pic.huff\n");
-			printf("Example 2: ./huffman uncompress pic.huff picture.png\n");
+			printf("Example 2: ./huffman decompress pic.huff picture.png\n");
 			printf("\nEnding program.\n");
 			break;
 		case 2:
@@ -48,12 +48,12 @@ int main(int argc, char const *argv[]){
 	}
 
 	/*
-	 * Uncompress the file if the 'uncompress'
+	 * Decompress the file if the 'decompress'
 	 * argument is used.
 	*/
-	else if ( !strcmp(argv[1], "uncompress") ){
-		printf("Uncompressing file...\n");
-		//uncompress(argv[2], argv[3]);
+	else if ( !strcmp(argv[1], "decompress") ){
+		printf("Decompressing file...\n");
+		//decompress(argv[2], argv[3]);
 	}
 	//Invalid argument
 	else
