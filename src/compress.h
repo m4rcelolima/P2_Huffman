4e6b2a462175fe695f72e3b302f2e41238b0a5e8
Marcelo Lima <marcelolima@ic.ufal.br>
2018-05-14 16:11:18 -0300

    Add huffman-conding function headers

--- src/compress.h
+++ src/compress.h
@@ -3,3 +3,7 @@
 void compress(unsigned char*, long int, const char*);
 
 node *createHuffmanTree(unsigned char *file_array, long int file_size);
+
+void mapHuffman(node *huffman_tree, node *coding[], char *bits, char *nextBit);
+
+void addHuffmanCoding(node **coding, char item);
