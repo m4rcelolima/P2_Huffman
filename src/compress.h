46057185135aa21f0a5b73d7f0ad6f4305e62533
Marcelo Lima <marcelolima@ic.ufal.br>
2018-05-15 11:38:30 -0300

    Add write_tree header

--- src/compress.h
+++ src/compress.h
@@ -2,6 +2,8 @@
 
 void compress(unsigned char*, long int, const char*);
 
+void write_tree(node* huffman_tree, FILE* compressed_file, int* tree_size);
+
 node *createHuffmanTree(unsigned char *file_array, long int file_size);
 
 void mapHuffman(node *huffman_tree, node *coding[], char *bits, char *nextBit);
